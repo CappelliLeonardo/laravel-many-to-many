0e847db5bfab41a22f9ac9910db65f478de00693
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-03-09 14:21:36 +0100

    Milestone 1.2.3.4 completate

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+//Models
+use App\Models\Technology;
+//Helpers
+use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\Stmt\Foreach_;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Technology::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $allTechnologies=[
+            'News',
+            'Updates',
+            'Release',
+            'Technology',
+            'Web',
+            'Software',
+            'Hardware',
+            'Blockchain',
+            'AI',
+            'Machine Learning',
+            'ChatGPT',
+        ];
+
+        foreach ($allTechnologies as $singleTechnology) {
+
+            $technology = Technology::create([
+                'title' => $singleTechnology,
+                'slug' => str()->slug($singleTechnology),
+            ]);
+        }
+    }
+}
